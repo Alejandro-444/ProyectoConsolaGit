34c20829554713e7a4089faa3bc4ec2d6731f333
Alejandro-444 <bonoseguraalejandro@gmail.com>
2024-12-03 12:14:12 +0100

    archivo operaciones creado

---
+++ src/operaciones.java
@@ -0,0 +1,8 @@
+public class operaciones {
+    public static void main(String[] args) {
+        int num1=1;
+        int num2=2;
+        int multiplicacion =num1*num2;
+        int division = num2/num1;
+    }
+}
